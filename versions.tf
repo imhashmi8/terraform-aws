59f723abe552ea8ff68ad34aed67a0ead8814fcb
Md Qamar Hashmi <qamar.hashmi@outlook.com>
2024-05-24 02:58:27 +0530

    added terraform providers

---
+++ versions.tf
@@ -0,0 +1,14 @@
+#### Terraform Block 
+
+terraform {
+  required_version = "~>1.8"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.5"
+
+    }
+  }
+}
+
+###
